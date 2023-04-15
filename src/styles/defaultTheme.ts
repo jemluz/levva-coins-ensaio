e0ab22b53b04cb87d090f93f4a698168c3552afe
Jemima <contatojemima@gmail.com>
2023-04-15 17:18:37 -0300

    Estruturando a aplicação - Fontes e estilos
    - Importação da fonte Roboto
    - Creiação do defaultTheme.ts
    - Criação da interface para defaultTheme
    - Adição de ThemeProvider no App( )
    - Vinculo do ThemeProvider com o defaultTheme
    - Criação do global.ts

---
+++ src/styles/defaultTheme.ts
@@ -0,0 +1,15 @@
+export const defaultTheme = {
+  white: '#fff',
+
+  'gray-300': '#C4C4CC',
+  'gray-400': '#7C7C8A',
+  'gray-500': '#343435',
+  'gray-600': '#282829',
+
+  'black': '#1D1D1D',
+
+  'yellow-300': '#F2D026',
+  'yellow-500': '#FFC600',
+
+  'red-500': '#F75A68',
+} as const

fee57933c301ef8a1907cb0b05fad92b258e2229
Jemima <contatojemima@gmail.com>
2023-04-16 18:53:42 -0300

    Feature/router (#1)
    * Roteamento - Criando o roteador
    
    - npm i -D react-router-dom
    - Criação do Router.tsx
    - Mapeamento das rotas
    
    * Roteamento - Vinculando as rotas
    
    - Criação do componente /pages/Home
    - Criação do componente /pages/NewAccount
    - Criação do componente /pages/Login
    - Vinculo dos componentes de /pages com suas respectivas rotas, dentro do Router.tsx
    - Utilização do provedor BrowserRouter no App( )
    - Vinculo do Router( ) com o App( )

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/defaultTheme'
 import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div className="App">
-        Qual é
-      </div>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )

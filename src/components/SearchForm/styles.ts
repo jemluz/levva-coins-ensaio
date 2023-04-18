db95a637b0ef54b9a67386108943bf63b3b7be1f
Jemima <contatojemima@gmail.com>
2023-04-18 00:45:09 -0300

    Estilização - Barra de pesquisa II
    - Estilização do SearchFormContainer.
    - Estilização do input.

--- src/components/SearchForm/styles.ts
+++ src/components/SearchForm/styles.ts
@@ -1,6 +1,26 @@
 import styled from "styled-components";
 
 export const SearchFormContainer = styled.form`
-  grid-column: 2;
-  margin-top: 1.5rem;
+  grid-column: 2 / 3;
+  margin-top: 3rem;
+
+  max-width: 70rem;
+  display: grid;
+  grid-template-columns: 1fr 1.5rem auto;
+
+  input {
+    grid-column: 1 / 2;
+    flex: 1;
+    border-radius: 6px;
+    border: 0;
+    background: ${props => props.theme["gray-700"]};
+    color: ${props => props.theme["gray-300"]};
+    padding: 1rem;
+
+    &::placeholder {
+      color: ${props => props.theme["gray-500"]};
+    }
+  }
+
+  
 `;

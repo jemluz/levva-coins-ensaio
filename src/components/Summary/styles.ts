44220ef171ecda2e3fe23d09333d2c845fe63ba3
Jemima <contatojemima@gmail.com>
2023-04-17 23:48:43 -0300

    Estilização - Sumário I
    - npm i -D phosphor-react
    - Criação do arquivo /components/Summary/index.tsx
    - Criação do arquivo /components/Summary/styles.ts
    - Marcação dos styled components dentro do Summary( ).
    - Adição do componente Summary( ) na página Home( ).

---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,9 @@
+
+import styled from "styled-components"
+
+export const SummaryContainer = styled.section`
+  grid-column: 2;
+`
+
+export const SummaryCard = styled.div`
+`
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
 import { HomeWrapper } from "./styles";
 
 export function Home() {
   return (
     <HomeWrapper>
       <Header />
+      <Summary />
     </HomeWrapper>
   )
 }

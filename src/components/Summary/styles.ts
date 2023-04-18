8248df610ba89b58e4b734a5e9a79b021d8f2d1a
Jemima <contatojemima@gmail.com>
2023-04-17 23:54:25 -0300

    Estilização - Sumário I
    - Estilização do SummaryContainer.

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -3,6 +3,16 @@ import styled from "styled-components"
 
 export const SummaryContainer = styled.section`
   grid-column: 2;
+
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+  margin-top: -5rem;
 `
 
 export const SummaryCard = styled.div`

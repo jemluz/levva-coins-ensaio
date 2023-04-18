62f7ee20b8f5d973f5704475429e724c787d155e
Jemima <contatojemima@gmail.com>
2023-04-18 01:41:51 -0300

    Estilização - Modal layout V
    - Estilização do Overlay.

--- src/layouts/Modal/styles.ts
+++ src/layouts/Modal/styles.ts
@@ -2,6 +2,11 @@ import * as Dialog from "@radix-ui/react-dialog";
 import styled from "styled-components";
 
 export const Overlay = styled(Dialog.Overlay)`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  inset: 0;
+  background: #00000075;
 `
 
 export const Content = styled(Dialog.Content)`

05ea4dffb0839cd85f16c36e341f0fdf77479948
Jemima <contatojemima@gmail.com>
2023-04-17 23:32:58 -0300

    Estilização - Header II
    - Estilização do HeaderContainer.

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,14 @@
 import styled from "styled-components";
+
 export const HeaderContainer = styled.header`
+  background: ${(props) => props.theme["gray-600"]};
+  padding: 2.5rem 0 7.5rem;
+
+  grid-column-start: 2;
+  grid-column-end: 3;
+
+  display: grid;
+  grid-template-columns: 1fr 70rem 7rem 1fr;
 `;
 export const HeaderContent = styled.div`
 export const NewCategoryButton = styled.button`

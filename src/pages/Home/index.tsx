3f7820c50fe7288c5b59431c702cb9135c2f912d
Jemima <contatojemima@gmail.com>
2023-04-20 00:53:20 -0300

    Estilização - Tabela I
    - Criação dos styled components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import { Header } from "../../components/Header";
 import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
-import { HomeWrapper } from "./styles";
+import { HomeWrapper, PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Home() {
   return (
@@ -9,6 +9,32 @@ export function Home() {
       <Header />
       <Summary />
       <SearchForm />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight>R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hambúrguer</td>
+              <td>
+                <PriceHighlight>- R$ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </HomeWrapper>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,3 +4,13 @@ export const HomeWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 77rem 1fr;
 `;
+
+export const TransactionsContainer = styled.main`
+  grid-column: 2 / 3;
+`
+
+export const TransactionsTable = styled.table`
+`
+
+export const PriceHighlight = styled.span`
+`
